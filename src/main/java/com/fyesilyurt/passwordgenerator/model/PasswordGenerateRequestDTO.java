cb3801da66e69c1cf76d07832e3cc605998bd44b
furkanyesilyurt <52417156+furkanyesilyurt@users.noreply.github.com>
2023-01-02 22:24:04 +0300

    data annotation added

--- src/main/java/com/fyesilyurt/passwordgenerator/model/PasswordGenerateRequestDTO.java
+++ src/main/java/com/fyesilyurt/passwordgenerator/model/PasswordGenerateRequestDTO.java
@@ -5,10 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class PasswordGenerateRequestDTO {
 
     private boolean hasUpperCase;
