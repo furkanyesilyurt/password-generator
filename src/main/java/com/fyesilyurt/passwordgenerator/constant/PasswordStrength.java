bb8e5e7f80e0c4cdffe5485a057ab6fd38977eb7
furkanyesilyurt <52417156+furkanyesilyurt@users.noreply.github.com>
2023-01-04 22:38:40 +0300

    checkPasswordStrength

--- src/main/java/com/fyesilyurt/passwordgenerator/constant/PasswordStrength.java
+++ src/main/java/com/fyesilyurt/passwordgenerator/constant/PasswordStrength.java
@@ -2,9 +2,7 @@ package com.fyesilyurt.passwordgenerator.constant;
 
 public enum PasswordStrength {
 
-    VERY_WEAK,
     WEAK,
     GOOD,
-    STRONG,
-    VERY_STRONG
+    STRONG
 }
--- src/main/java/com/fyesilyurt/passwordgenerator/service/GeneratorService.java
+++ src/main/java/com/fyesilyurt/passwordgenerator/service/GeneratorService.java
@@ -7,6 +7,8 @@ import com.fyesilyurt.passwordgenerator.model.PasswordGenerateResponseDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class GeneratorService {
@@ -21,7 +23,7 @@ public class GeneratorService {
     public PasswordGenerateResponseDTO generate(PasswordGenerateRequestDTO request) {
 
         String generatedPassword = generatePassword(request);
-        PasswordStrength strength = checkPasswordStrength(request);
+        PasswordStrength strength = checkPasswordStrength(generatedPassword);
         return PasswordGenerateResponseDTO.builder()
                 .generatedPassword(generatedPassword)
                 .passwordStrength(strength)
@@ -45,7 +47,19 @@ public class GeneratorService {
         return new String(password);
     }
 
-    public PasswordStrength checkPasswordStrength(PasswordGenerateRequestDTO requestDTO) {
-        return null;
+    public PasswordStrength checkPasswordStrength(String generatedPassword) {
+        String strongRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{10,}$";
+        String mediumRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}$";
+        Pattern strongPattern = Pattern.compile(strongRegex);
+        Matcher strongMatcher = strongPattern.matcher(generatedPassword);
+        Pattern mediumPattern = Pattern.compile(mediumRegex);
+        Matcher mediumMatcher = mediumPattern.matcher(generatedPassword);
+        if (strongMatcher.matches()) {
+            return PasswordStrength.STRONG;
+        } else if (mediumMatcher.matches()) {
+            return PasswordStrength.GOOD;
+        } else {
+            return PasswordStrength.WEAK;
+        }
     }
 }
